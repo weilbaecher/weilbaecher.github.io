3e7c8f43bfbab1761a4997d91b81edf99af4bdad
nolaandy <andyw504@gmail.com>
2017-03-22 16:56:42 -0500

    more changes

--- OneSignalSDKUpdaterWorker.js
+++ OneSignalSDKUpdaterWorker.js
@@ -1,2 +1,2 @@
-importScripts('https://weilbaecher.github.io/service-worker.js');
+importScripts('https://weilbaecher.github.io/service-worker2.js');
 importScripts('https://cdn.onesignal.com/sdks/OneSignalSDK.js');
