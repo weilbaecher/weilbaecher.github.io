03bb68ac8732f12a65ddf926da179e18da40030d
nolaandy <andyw504@gmail.com>
2017-03-23 17:26:08 -0500

    Updating OneSignal

--- service-worker2.js
+++ service-worker2.js
@@ -1,4 +1,4 @@
-var cacheTimestamp = '1490307099178';
+var cacheTimestamp = '1490307905698';
 var cacheName = 'andy-dev-shell-v' + cacheTimestamp;
 var dataCacheName = 'andy-dev-data-v' + cacheTimestamp;
 var filesToCache = [
